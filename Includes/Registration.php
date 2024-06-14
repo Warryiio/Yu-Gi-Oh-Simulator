df85d220d0b8f267290cfff196acd20a04b0aee9
XiaWa477 <XiaWa477@gmail.com>
2024-06-14 22:59:18 +0200

    register

--- Includes/Registration.php
+++ Includes/Registration.php
@@ -3,11 +3,30 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Register</title>
+    <link rel="stylesheet" href="/Yu-Gi-Oh-Simulator/CSS/Register.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script>
+        function redirectToPage() {
+            window.location.href = "/Yu-Gi-Oh-Simulator";
+        }
+    </script>
 </head>
 <body>
     <h1>Register</h1>
-    
+
+    <form>
+        <div class="register-container">
+            <input type="text" id="username" placeholder="Username">
+            <br>
+            <input type="text" id="Email" placeholder="E-Mail">
+            <br>
+            <input type="password" id="password" placeholder="Password">
+            <br>
+            <button onclick="redirectToPage()" type="button" class="button">Go Back</button>
+            <button type="submit" class="registerButton">Register</button>
+        </div>
+    </form>
     
 </body>
 </html>
--- index.php
+++ index.php
@@ -6,30 +6,26 @@
     <title>YU-GI-OH</title>
     <link rel="stylesheet" href="/Yu-Gi-Oh-Simulator/CSS/index.css">
     
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js">
-        document.getElementById("loginButton").onclick = function () {
-            location.href = "localhost/Yu-Gi-Oh-Simulator/CSS/welcome.php";
-        };
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script>
+        function redirectToPage() {
+            window.location.href = "/Yu-Gi-Oh-Simulator/Includes/Registration.php";
+        }
     </script>
 </head>
 <body>
     <h1>YU-GI-OH</h1>
-    <h2>Register</h2>
+    <br>
     <form class="" action="" method="post" autocomplete="off">
     <div class="input-container">
         <input type="text" id="usernameOrEmail" name="nameOrEmail" placeholder="Username or Email">
         <input type="password" id="password" name="password" placeholder="Password" > 
     </div>
     <div class="button-container">
-        <button class="button" id="loginButton" >Login</button>
-        <button onclick="redirectToPage()" class="buttonsignup" id="signupButton">Sign Up</button>
+        <button  type="submit" class="button" id="loginButton" >Login</button>
+        <button type="button" onclick="redirectToPage()" class="buttonsignup" id="signupButton">Sign Up</button>
     </div>
     </form>
 
-    <script>
-        function redirectToPage() {
-            window.location.href = "/Yu-Gi-Oh-Simulator/Includes/Registration.php";
-        }
-    </script>
 </body>
 </html>
