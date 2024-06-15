467fa3b675c15c1a050004df5b0c20e559d96927
XiaWa477 <XiaWa477@gmail.com>
2024-06-15 21:11:39 +0200

    register

--- Includes/Registration.php
+++ Includes/Registration.php
@@ -11,19 +11,28 @@
             window.location.href = "/Yu-Gi-Oh-Simulator";
         }
     </script>
+
 </head>
 <body>
-    <?php
-        include("/Yu-Gi-Oh-Simulator/php/connect.php");
+    <?php    
+        include ('php/connect.php');
         if(isset($_POST['submit'])) {
             $username= $_POST['username'];
             $email = $_POST['email'];
             $password =$_POST['password'];
 
-            $verify_query = mysqli_query($connection, "SELECT Email FROM User WHERE Email='$email")
-        }
+            $verify_query = mysqli_query($connection, "SELECT dtEmail FROM tblUsers WHERE dtEmail='$email'");
+
+            if(mysqli_num_rows($verify_query) !=0)  {
+                error();
+            } else {
+                mysqli_query($connection,"INSERT INTO tblUsers(dtUsername,dtEmail,dtPassword) VALUES('$username','$email','$password')") or die("Error Occured");
+                success();
+            }
+        } else {
 
     ?>
+
     <h1>Register</h1>
 
     <form>
@@ -38,6 +47,37 @@
             <button type="submit" class="registerButton">Register</button>
         </div>
     </form>
-    
+
+    <div class=popup id="popup">
+        <img id="status" height src="" width="150" />
+        <h2 id="message"></h2>
+        <p id="details"></p>
+        <button type="button" onclick="closePopup()">OK</button>
+    </div>
+    <?php }?>
+
+    <script>
+        let popup= document.getElementById("popup");
+    function openPopup() {
+        popup.classList.add("open-popup");
+
+    }
+    function closePopup() {
+        popup.classList.remove("open-popup");
+    }
+
+    function error() {
+        document.getElementById("status").src= "/Yu-Gi-Oh-Simulator/images/red.png";
+        document.getElementById("message").innerHTML = "Something went wrong";
+        document.getElementById("details").innerHTML = "Your Username or Email is already used";
+        openPopup();
+    }
+    function success() {
+        document.getElementById("status").src= "/Yu-Gi-Oh-Simulator/images/green.png";
+        document.getElementById("message").innerHTML = "Thank you";
+        document.getElementById("details").innerHTML = "Your Account has been created";
+        openPopup();
+    }
+    </script>
 </body>
 </html>
