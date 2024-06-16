ca4be777677395c547dcb4fd9db67984cfcf71ed
XiaWa477 <XiaWa477@gmail.com>
2024-06-16 04:47:31 +0200

    login css

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>YU-GI-OH</title>
-    <link rel="stylesheet" href="/Yu-Gi-Oh-Simulator/CSS/index.css">
+    <link rel="stylesheet" href="/Yu-Gi-Oh-Simulator/CSS/Login.css">
     
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script>
@@ -21,6 +21,7 @@
         <input type="text" id="email" name="email" placeholder="Email" required>
         <input type="password" id="password" name="password" placeholder="Password" required> 
     </div>
+    
     <div class="button-container">
         <button  type="button" onclick="openPopup()" name="submit" class="button" id="loginButton" >Login</button>
         <button type="button" onclick="redirectToPage()" class="buttonsignup" id="signupButton">Sign Up</button>
