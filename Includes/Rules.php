040cd87a74694d9fcaddd3ef28d66a3c10f6d39e
Dinis <131139324+Dp08122004@users.noreply.github.com>
2024-06-23 11:08:09 +0200

    Finished Rules.php

--- Includes/Rules.php
+++ Includes/Rules.php
@@ -68,13 +68,18 @@ body {font-family: "Lato", sans-serif;}
 
 <div id="Cards" class="tabcontent">
   <h3>Cards</h3>
-  <p>The cards </p>
+  <p>Cards are divided up in 3 sections: The top the middle and the bottom</p>
+  <p>On the top part there is the name and the stars of the card</p>
+  <p>On the middle there's the image and the attack (atk) and defence (def) stats points</p>
+  <p>At the bottom there are the cards abilities, which can range from none to multiple ones</p>
+  <p>The cards can be played in a defencive or attacking position</p>
+  <p>The cards can also be played upside-down with the back at the top</p>
 </div>
 
 <div id="Decks" class="tabcontent">
   <h3>Decks</h3>
   <p>The composed Decks are made up from 40 up to 60 different cards.</p>
-  <p>You can have the same card only up to 3 </p> 
+  <p>You can have the same card only up to 3 times per deck</p> 
   <p>Little advice: Try to have the smallest Deck possible as this reduces your chances to draw an unwanted card</p><br>
   <p style="color:Tomato;">For advanced players: You can have a extra deck that can hold maximum 15 cards</p> 
   <p style="color:Tomato;">It contains Xyz Monsters, Synchro Monsters and Fusion Monsters, in any combination.</p> 
@@ -86,7 +91,14 @@ body {font-family: "Lato", sans-serif;}
   <p>Note: This content doesn't resume every Rule of Yu-Gi-Oh, to have a complete version of the rules please click this link:</p>
   <a href="https://www.yugioh-card.com/en/rulebook/">Whole Ruleset</a>
   <p>The objective of Yu-Gi-Oh is simple ! </p>
-  <p>You can have the same card only up to 3 </p>
+  <p>You and your opponent have both 4000 health, the objective is to lower your opponents health to 0</p><br>
+  <p>The stars on the cards define how much sacrified cards are needed to be placed</p>
+  <p>From 1 to 4 stars: No sacrifice needed</p>
+  <p>From 5 to 8 stars: One sacrifice needed</p>
+  <p>From 9 to 13 stars: Two sacrifices needed</p><br>
+  <p>If a card agresses a card that has a higher attack it will be destroyed (in attacking position, flipped either way)</p>
+  <p>If a card agresses a card that has a higher defense it will be destroyed (in defensive position, flipped either way)</p>
+  <p>An attack on a empty field will reduce the opponents health directly from the health counter, a successful attack on a other card will destroy it</p>
 
 </div>
 
