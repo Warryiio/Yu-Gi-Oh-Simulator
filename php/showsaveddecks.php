1b69c995b41d3b583bcbade760ef76cc483124df
Luka <157367482+xKiLx7805@users.noreply.github.com>
2024-06-24 11:21:52 +0200

    BitCOin

---
+++ php/showsaveddecks.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+@include 'connect.php';
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $deck = $_POST['deck'];
+    $extraDeck = $_POST['extraDeck'];
+    $deck_name = $_POST['deck_name'];
+    $image = $_POST['image'];
+    $username = $_SESSION['username'];
+    $stmt = $connection->prepare("INSERT INTO tblOpenDecks (dtUsername, dtDeckName, dtCards, dtExtraCards , dtImage) VALUES (?, ?, ?, ?, ?)");
+    $stmt->bind_param("sssss", $username, $deck_name, $deck, $extraDeck , $image);
+    $stmt->execute();
+    $stmt->close();
+    
+}
+
+$connection->close();
+?>
